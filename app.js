4390a5615698fb74ada7912b9b84419dea179c6d
imranshahzad17 <imranshahzad3454@gmail.com>
2024-02-24 09:59:53 +0500

    task done

---
+++ app.js
@@ -0,0 +1,62 @@
+// Q1:Get user to input a number using prompt()check if the number of 6 or not ?
+
+// let Num = +prompt("ENTER YOUR NUMBER");
+
+// if(Num%6){
+//     document.write("NO YOUR NUMBER IS NOT 6")
+// }
+// else{
+//     document.write("YES YOUR NUMBER IS 6")
+// }
+
+// ===========================================================================================
+
+// Q2:Write a code which can give grades to student according to their scores?
+
+// let score = +prompt("ENTER YOUR SCORE");
+// let grades;
+
+// if(score >=90 && score <=100){
+//     document.write("GRADE = A+")
+// }else if(score >=70 && score <=89){
+//     document.write("GRADE = A")
+// }else if(score >=50 && score <=69){
+//     document.write("GRADE = B")
+// }else if(score >=40 && score <=49){
+//     document.write("GRADE = C")
+// }else if(score >=0 && score <=39){
+//     document.write("FAIL")
+// }
+
+// ==========================================================================================
+
+// Q3: Create a marksheet using if else and else if condition? 
+
+// var name = prompt("Enter Your Name");
+// var eng = +prompt("English Marks")
+// var urdu = +prompt("Urdu Marks")
+// var math = +prompt("Math Marks")
+// var sci = +prompt("Science Marks")
+// var isl = +prompt("Islamiat Marks")
+
+// var total = eng + urdu + math + sci + isl;
+// var percentage =(total / 500 ) * 100 ;
+
+// if(percentage >=90 && percentage <=100){
+//     console.log( name + " Your grade is A+ " + percentage + "%");
+// }
+// else if(percentage >=70 && percentage <=89){
+//     console.log( name + " Your grade is A " + percentage + "%");
+// }
+// else if(percentage >=50 && percentage <=69){
+//     console.log( name + " Your grade is B " + percentage + "%");
+// }
+// else if(percentage >=40 && percentage <=49){
+//     console.log( name + " Your grade is C " + percentage + "%");
+// }
+// else if(percentage >=0 && percentage <=39){
+//     console.log( name + "FAIL " + percentage + "%");
+// }
+// else{
+//     console.log("Invalid Number")
+// }
